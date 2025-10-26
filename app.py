c9521af1893c254e3d9979a6c5b839599ac7786d
wilsonstefani <wilsonstefani@hotmail.com>
2025-10-26 20:35:57 -0300

    Ajuste do app para deploy Railway

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway define a porta via variável de ambiente
+    app.run(host="0.0.0.0", port=port)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
